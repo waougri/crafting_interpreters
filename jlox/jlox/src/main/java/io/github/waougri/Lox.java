101d15f36c88650fedcbacdf7dc02eb1de59a9c5
Ayman Ougri <aymanougri@aymans-mbp.lan>
2025-09-16 14:31:37 -0400

    4.6.1 - Scanners, Token, TokenType, Scanner::string()

---
+++ jlox/jlox/src/main/java/io/github/waougri/Lox.java
@@ -0,0 +1,55 @@
+package io.github.waougri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+  static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  // fire up the interpreter
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+    if (hadError)
+      System.exit(65);
+  }
+
+  // repl
+  private static void runPrompt() throws IOException {
+    var input = new InputStreamReader(System.in);
+    var reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.println("[jlox]> ");
+      var line = reader.readLine();
+      if (line == null)
+        break;
+      run(line);
+    }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error " + where + ": " + message);
+    hadError = true;
+  }
+}
